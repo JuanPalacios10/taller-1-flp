11b3a7344f8d2522e6a374c3a134d13dbc7bd38c
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2024-09-21 22:35:19 -0500

    Implementacion de datatypes en parser-unparser

--- parser-unparser.rkt
+++ parser-unparser.rkt
@@ -1,3 +1,23 @@
 #lang eopl
 
 ;; Autores: Juan Miguel Palacios Doncel, 2359321. Yeifer Ronaldo Muñoz Valencia, 227868665. Juan Carlos Rojas Quintero, 2359358
+
+(define-datatype circuito circuito?
+  (simple-circuit (in (list-of symbol?)) (out (list-of symbol?)) (chip chip?))
+  (complex-circuit (circ circuito?) (lcircs (list-of circuito?)) (in (list-of symbol?)) (out (list-of symbol?)))
+)
+
+(define-datatype chip chip?
+  (prim-chip (chip-prim chip-prim?))
+  (comp-chip (in (list-of symbol?)) (out (list-of symbol?)) (circ circuito?))
+)
+
+(define-datatype chip-prim chip-prim?
+  (chip_or)
+  (chip_and)
+  (chip_not)
+  (chip_xor)
+  (chip_nand)
+  (chip_nor)
+  (chip_xnor)
+)
